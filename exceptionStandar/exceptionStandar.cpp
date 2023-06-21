faf52d96e5999828ba37fe710ca27a9f6711de2a
MuhammadAkramsyahRijja <127277578+MuhammadAkramsyahRijja@users.noreply.github.com>
2023-06-21 12:00:20 +0700

    exceptionStandar.cpp

--- exceptionStandar/exceptionStandar.cpp
+++ exceptionStandar/exceptionStandar.cpp
@@ -1,7 +1,7 @@
 include <iostream> 
 #include <execution>
 //untuk obyek exception yang akan digunakan
-#include <array>
+#include <array> 
 //untuk obyek array yang akan kita gunakan 
 using namespace std;
 
